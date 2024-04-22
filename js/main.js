a5e27b1620fd2b5bb3a5f91f9bdd3d266223e683
Karimov-08 <karimovabdurahim1877@gmail.com>
2024-04-22 16:25:45 +0500

    first reposity

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elBurger = document.querySelector(".burger");
+var elHeaderBox = document.querySelector(".header-box");
+var elClose = document.querySelector(".close-img");
+
+elBurger.addEventListener("click", function() {elHeaderBox.classList.add('header-show')})
+elClose.addEventListener("click", function() {elHeaderBox.classList.remove('header-show')})
